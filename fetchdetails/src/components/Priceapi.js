4eb6003b023948b23f7c6695e6c93f569243a6fa
lekhana-g <lekhana.ginjupalli@fareportal.com>
2022-02-23 05:23:49 -0500

    fetch details using api1

---
+++ fetchdetails/src/components/Priceapi.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import Axios from "axios"
+
+export class Priceapi extends Component {
+    constructor() {
+        super();
+        this.state = {
+            pricedata: {}
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1 style={{textAlign:'center',color:'lightslategray'}}>SBI Equity cash</h1><hr/>
+                <div className="card center" style={{"padding":"10px","margin":"30px",width:'50%',align:'center'}}>
+                    <div className="card-header">
+                       <b>{this.state.pricedata.HN}</b><br/>
+                       <b>{this.state.pricedata.SC_FULLNM}</b><br/>
+                       <b>Sector:- {this.state.pricedata.DISPTYP}</b>
+                       <br/><b> IFSC start id:- {this.state.pricedata.NSEID}</b>
+                    </div>
+                    <div class="card-text" style={{paddingInlineStart:'10px',padding:'15px'}}>
+                        <b>Current Price: </b>{this.state.pricedata.pricecurrent}<br/>
+                        <b> Previous Price : </b> {this.state.pricedata.priceprevclose}
+                        <br/>
+                        <b> Total sell Quantity : </b> {this.state.pricedata.tot_sell_qty}
+                        <br/>
+                        <b> Price change : </b> {this.state.pricedata.pricechange}
+                        <br/>
+                        <b> Price Percentage change(%) : </b> {this.state.pricedata.pricepercentchange}
+                        <br/>
+                        <b> Market cap: </b> {this.state.pricedata.MKTCAP}
+                        <br/>
+                    </div>
+
+                    
+                    </div>
+
+            </div>
+        )
+    }
+    componentDidMount(){
+        var dataPromise=Axios.get("https://priceapi.moneycontrol.com/pricefeed/bse/equitycash/SBI")
+        dataPromise.then((response) => {
+            this.setState({pricedata: response.data.data})
+        })
+    }
+}
+
+export default Priceapi
--- fetchdetails/src/index.js
+++ fetchdetails/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FlightList from './components/FlightList' 
+import Priceapi from './components/Priceapi'
 
 ReactDOM.render(
   <div>
-   <FlightList/>
+        <Priceapi/>
+   {/*<FlightList/>*/}
   </div>
   ,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
