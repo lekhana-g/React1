b1c2aecc06e87176fcf1ce092abe4d22b3fa6b20
lekhana-g <lekhana.ginjupalli@fareportal.com>
2022-02-25 22:55:51 -0500

    single page application

--- singlepageapp/src/index.js
+++ singlepageapp/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom'
-import { BrowserRouter,Route} from "react-router-dom";
+import { BrowserRouter,Route } from "react-router-dom";
 import {Link} from "react-router-dom"
 import MainComponent from './components/MainComponent'
 import UpdateComponent from './components/UpdateComponent'
@@ -15,15 +15,15 @@ function App() {
          <div>
          <h1 style={{textAlign:"center",paddingTop:"10px"}}>CRUD operation</h1>
           <nav>
-              <Link style={{margin: "10px",paddingLeft:"480px"}} to="/">Product Details</Link>
+              <Link exact style={{margin: "10px",paddingLeft:"480px"}} to="/">Product Details</Link>
               <Link style={{margin: "10px"}} to="/Add">Add new product</Link>
               <Link style={{margin: "10px"}} to="/Update/id">Update product</Link>
               <hr/>
           </nav>
           <Routes>
-            <Route exact path="/" element={MainComponent()}></Route>
-            <Route exact path="/Update/id" element={UpdateComponent()}></Route>
-            <Route exact path="/Add" element={AddComponent()}></Route>
+                <Route path="/Update/:id" element={UpdateComponent()}></Route>
+                <Route path="/Add" element={AddComponent()}></Route>
+                <Route path="/" element={MainComponent()}></Route>
           </Routes>
          </div>
       </BrowserRouter>
